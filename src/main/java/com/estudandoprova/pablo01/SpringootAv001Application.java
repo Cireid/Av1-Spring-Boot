ff8c71ae4a2c618d6af824d7945eb1875b3de958
Cireid <gabrieldieric@gmail.com>
2018-03-22 09:12:44 -0300

    Primeira Parte da Avaliação

---
+++ src/main/java/com/estudandoprova/pablo01/SpringootAv001Application.java
@@ -0,0 +1,12 @@
+package com.estudandoprova.pablo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringootAv001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringootAv001Application.class, args);
+	}
+}
